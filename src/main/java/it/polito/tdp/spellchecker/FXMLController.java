30594655a7aa16b4533b84755b87838f21ba12ed
nMaax <carli.massimiliano01@gmail.com>
2023-03-21 13:48:50 +0100

    Defined FXMLController Skeleton

--- src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -7,6 +7,10 @@ package it.polito.tdp.spellchecker;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class FXMLController {
 
@@ -16,10 +20,37 @@ public class FXMLController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
+    @FXML // fx:id="btClearText"
+    private Button btClearText; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnSpellCheck"
+    private Button btnSpellCheck; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cmbChooseLanguage"
+    private ComboBox<?> cmbChooseLanguage; // Value injected by FXMLLoader
+
+    @FXML // fx:id="lblErrors"
+    private Label lblErrors; // Value injected by FXMLLoader
+
+    @FXML // fx:id="lblPerformance"
+    private Label lblPerformance; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtAreaUserInput"
+    private TextArea txtAreaUserInput; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtAreaWrongWords"
+    private TextArea txtAreaWrongWords; // Value injected by FXMLLoader
+
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
-    	
+        assert btClearText != null : "fx:id=\"btClearText\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnSpellCheck != null : "fx:id=\"btnSpellCheck\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert cmbChooseLanguage != null : "fx:id=\"cmbChooseLanguage\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert lblErrors != null : "fx:id=\"lblErrors\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert lblPerformance != null : "fx:id=\"lblPerformance\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtAreaUserInput != null : "fx:id=\"txtAreaUserInput\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtAreaWrongWords != null : "fx:id=\"txtAreaWrongWords\" was not injected: check your FXML file 'Scene.fxml'.";
+
     }
+
 }
-
-
